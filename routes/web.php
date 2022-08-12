b3862543684fa0aec6470967bddd285bdf10ebf5
canh2 <christina.g.amine@gmail.com>
2022-08-12 11:36:29 +0300

    cm

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,12 @@ Route::get('/', function () {
 });
 
 Route::get('/register',[UserController::class,'create']);
-
+Route::get('/admin',[AdminController::class,'index']);
 Route::post('/users',[UserController::class, 'store']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
